dad701466ee4daeba19bcf4e4dac171f712a2b7a
mcook2323 <mattcook2323@gmail.com>
2023-12-27 13:03:30 -0500

    changed the API to Mineable Coins and accessed BTC, ETH, and DOGE

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
-var coinmarketcapAPI = '';
+// var APIKey = '7bdb779c-f436-4e72-859f-e1dc48ade0d9';
+// var ApiKey = 'CB-ACCESS-KEY=OaDA814IfqvRqUYg'
+// const Url = 'https://api-public.sandbox.exchange.coinbase.com?'
+
+
 
 var con2coin = $('#con2coin');
 var con2dol = $('#con2dol');
 
-// check converter user input
+
+
+
+
+
+const Url = 'https://api.minerstat.com/v2/coins?list=BTC,ETH,DOGE,'
+
+function getData() {
+    fetch(Url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+ 
+    })
+    .catch(error => {
+        console.log('There was a problem with the fetch operation', error);
+    });
+}
+
+getData();
+
 
 if (con2coin.value && con2dol.value) {
     // alert the user only one input allowed
@@ -24,3 +53,7 @@ $( function() {
   } );
 
   
+
+  
+
+ 
