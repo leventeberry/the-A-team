d7883bd103d9cdbba3cfc86b92e23d335b112ce5
hokage-216 <berrylevente@gmail.com>
2024-01-09 09:23:11 -0500

    updated the history functions, also moved some code around

--- js/script.js
+++ js/script.js
@@ -10,16 +10,91 @@ const coinNames  = {
   SOL: 'Solana'
 };
 
+const searchHistory = [];
+
 //Accordion Function
 $( function() {
   $( "#accordion" ).accordion({
       heightStyle: "content",
-
       collapsible:true
   });
 } );
 
-//Get Price Function
+// set the search history in local storage
+function setSearchHistory(searchHistory) {
+  try {
+      const historyJson = JSON.stringify(searchHistory);
+      localStorage.setItem('searchHistory', historyJson);
+  } catch (error) {
+      console.error('Failed to save search history:', error);
+  }
+}
+
+// get the search history from local storage
+function getSearchHistory() {
+  try {
+      const historyJson = localStorage.getItem('searchHistory');
+      return historyJson ? JSON.parse(historyJson) : [];
+  } catch (error) {
+      console.error('Failed to retrieve search history:', error);
+      return [];
+  }
+}
+
+//History Funtions
+function showHistory() {
+  var resultsArea = $('#results-area');
+  resultsArea.empty();
+
+  // Create history container
+  var historyContainer = $('<div></div>').attr({id: "history-results-container"});
+  resultsArea.append(historyContainer);
+
+  var historyTitle = $('<div></div>').attr({id: "history-title"}).append('<h3>Your History:</h3>');
+  historyContainer.append(historyTitle);
+
+  var historyHeaders = $('<div></div>').attr({id: "history-headers"});
+  historyContainer.append(historyHeaders);
+
+  historyHeaders.append($('<span></span>').attr({id: "order-head"}).text('Order'));
+  historyHeaders.append($('<span></span>').attr({id: "action-head"}).text('Action'));
+
+  var historyList = $('<ol></ol>').attr({id: "history-list"});
+  historyContainer.append(historyList);
+
+  var history = getSearchHistory();
+
+  if (history.length === 0) {
+    historyList.append('<li>No history found</li>');
+  } else {
+    for (var i = 0; i < history.length; i++) {
+      var listItem = $('<li></li>');
+      var formattedTimestamp = dayjs(history[i].timestamp).format('MM/DD/YYYY @ hh:mm A [EST]'); // Format timestamp
+
+      if (history[i].type === "Price") {
+          listItem.text(`Price: ${history[i].coin} (${history[i].price}) (${formattedTimestamp})`);
+      } else if (history[i].type === "Comparison") {
+          listItem.text(`Compared: ${history[i].coin1} to ${history[i].coin2} (${formattedTimestamp})`);
+      } else if (history[i].type === "Converted") {
+          listItem.text(`Converted: ${history[i].input} to ${history[i].output} (${formattedTimestamp})`);
+      }
+
+      historyList.append(listItem);
+    }
+  }
+}
+ 
+
+function hideHistory() {
+  var historyContainer = $('#results-area');
+  historyContainer.empty();
+}
+
+function clearHistory() {
+  hideHistory();
+  searchHistory.length = 0;
+  localStorage.clear('searchHistory');
+}
 
 //query selector 
 var coinBtn = document.querySelector("#coin-selection");
@@ -30,22 +105,32 @@ coinBtn.addEventListener("click", getCryptoPrices);
 
 // function cryptoprices will retrieve coin value from api.
 function getCryptoPrices() {
-    var Cryptocoin = document.getElementById("cryptoSelection").value;
-    var Urlcoin = `https://min-api.cryptocompare.com/data/price?fsym=${Cryptocoin}&tsyms=USD`;
+  var Cryptocoin = document.getElementById("cryptoSelection").value;
+  var Urlcoin = `https://min-api.cryptocompare.com/data/price?fsym=${Cryptocoin}&tsyms=USD`;
 
-    fetch(Urlcoin)
-        .then(response => response.json())
-        .then(data => {
-           
-            var price = data.USD;
-          // display users selected coin with coin name and price
-          document.getElementById("results-area").innerHTML=`Crypto Prices`;
-            document.getElementById("results-area").innerHTML =`${Cryptocoin} Current Price: $${price} `;
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            document.getElementById("results-area").innerHTML = 'Error fetching data';
-        });
+  fetch(Urlcoin)
+    .then(response => response.json())
+    .then(data => {
+      const price = data.USD;
+      //create search object
+      const userPriceSearch = {
+        type : "Price",
+        price: price,
+        coin: Cryptocoin,
+        timestamp: dayjs().format('MM/DD/YYYY @ h:mm A')
+      }
+      // display users selected coin with coin name and price
+      document.getElementById("results-area").innerHTML=`Crypto Prices`;
+      document.getElementById("results-area").innerHTML =`${Cryptocoin} Current Price: $${price} `;
+      //add search to the history
+      searchHistory.push(userPriceSearch);
+      //add search to localstorage
+      setSearchHistory(searchHistory);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      document.getElementById("results-area").innerHTML = 'Error fetching data';
+    });
 }
 
 
@@ -106,7 +191,7 @@ async function fetchCoinInfo(coin) {
               const marketCap = data.RAW[coin].USD.MKTCAP;
               const volume24h = data.RAW[coin].USD.VOLUME24HOUR;
               const low24hour  = data.RAW[coin].USD.LOW24HOUR;
-              return { price, marketCap, volume24h, low24hour }; //
+              return { price, marketCap, volume24h, low24hour };
           }
           return null;
     default:
@@ -123,6 +208,18 @@ function compareCoins() {
     const coin2 = document.getElementById('coin2').value;
     Promise.all([fetchCoinInfo(coin1), fetchCoinInfo(coin2)])
       .then(([info1, info2]) => {
+        const userCompare = {
+          coin1 : coin1,
+          coin1_price : info1.price,
+          coin2 : coin2,
+          coin2_price : info2.price,
+          type : "Comparison",
+          timestamp: dayjs().format('MM/DD/YYYY @ hh:mm A')
+        };
+        //add search to the history
+        searchHistory.push(userCompare);
+        //add search to localstorage
+        setSearchHistory(searchHistory);
         const comparisonResult = document.getElementById('results-area');
         comparisonResult.innerHTML = '';
         if(coin1 === coin2){
